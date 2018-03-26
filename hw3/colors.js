542fd4fef5baa44c2f0529a5872228496d1ca0a3
OlyaZasimovich <olechka-zosya@mail.ru>
2018-03-26 23:07:13 +0300

    colors right variant

--- hw3/colors.js
+++ hw3/colors.js
@@ -11,23 +11,15 @@ function mood(colorsCount) {
   console.log( 'цветов: ' + colorsCount );
   let myColors = {};
 
-  for ( var i=1; i<=colorsCount; i++ )  {
+  for ( let i=1; i<=colorsCount; i++ )  {
     let n=randomDiap(1,7);
     let colorName=colors[n];
-    let availability = colorName in myColors;
-    if (availability) {
-      while (myColors[colorName] == colorName) {
-        n=randomDiap(1,7);
-        colorName=colors[n];
-      }
-    }
     myColors[colorName] = colorName;
   }
 
   for (let key in myColors) {
     console.log(myColors[key]);
-
   }
 }
 
-mood(3);
+mood(7);
